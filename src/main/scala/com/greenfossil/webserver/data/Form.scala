fffbbaac6b5ab89e30a374bdc0e39880a61cfad4
Cheong Chung Onn <chungonn@greenfossil.com>
2022-03-05 15:53:33 +0800

    Interim commit Form.bind wip

--- src/main/scala/com/greenfossil/webserver/data/Form.scala
+++ src/main/scala/com/greenfossil/webserver/data/Form.scala
@@ -3,28 +3,34 @@ package com.greenfossil.webserver.data
 import com.greenfossil.commons.json.JsValue
 import com.linecorp.armeria.common.HttpMethod
 
-type FormTupleMappings[Xs] <: Tuple = Xs match {
-  case EmptyTuple => EmptyTuple
-  case (String, Field[a]) *: xs => a *: FormTupleMappings[xs]
-}
-
-type FormClassMappings[Xs] <: Tuple = Xs match {
-  case EmptyTuple => EmptyTuple
-  case (String, Field[a]) *: xs => a *: FormTupleMappings[xs]
-}
-
-
 object Form {
 
-//  def apply[A](mappings: A): Form[MappingsTuple[A]] = ???
+  type FormMappings[Xs <: Tuple] <: Tuple = Xs match {
+    case EmptyTuple => Xs
+    case Field[t] *: ts => t *: FormMappings[ts]
+    case (String, Field[t]) *: ts => t *: FormMappings[ts]
+  }
 
-  def asTuple[A](mappings: A): Form[FormTupleMappings[A]] =
-    Form[FormTupleMappings[A]](mappings.asInstanceOf[Tuple])
+//  type FormClassMappings[Xs] <: Tuple = Xs match {
+//    case EmptyTuple => EmptyTuple
+//    case (String, Field[a]) *: xs => a *: FormTupleMappings[xs]
+//  }
+
+  //  def apply[A](mappings: A): Form[MappingsTuple[A]] = ???
+
+  def fields[A <: Field[_] *: Tuple](fields: A): Form[FormMappings[A]] =
+    Form[FormMappings[A]](fields)
+
+  def asTuple[A  <: (String, Field[_]) *: Tuple](tuple: A): Form[FormMappings[A]] =
+    val fs = tuple.map[[X] =>> Field[_]]([X] => (x: X) => x match {
+      case (name: String, f: Field[_]) => f.copy(name = name)
+    })
+    Form[FormMappings[A]](fs)
 
   import scala.deriving.Mirror
   import scala.compiletime.{summonFrom, error, constValue, erasedValue, summonInline}
 
-  inline def asClass[A](mappings: Tuple)/*(using m: Mirror.Of[A])*/: Form[A] = {
+  inline def asClass[A](mappings: Field[_] *: Tuple)/*(using m: Mirror.Of[A])*/: Form[A] = {
 //    val fields = toMappingFields(mappings)
     val m = summonInline[Mirror.ProductOf[A]]
     println(s"m = ${m}")
@@ -68,25 +74,22 @@ object Form {
       case _: Long => longNumber
       case _: String => text
     }
-
 }
 
-case class Form[A](mappings: Tuple, data: Map[String, Any] = Map.empty , errors: Seq[FormError] = Nil, value: Option[A] = None) {
+case class Form[A](mappings: Field[_] *: Tuple, data: Map[String, Any] = Map.empty, errors: Seq[FormError] = Nil, value: Option[A] = None) {
 
   import scala.deriving.Mirror
 
   def fill(values: A): Form[A] =
-    val data = values match {
+    val filledFields  = values match {
       case _values: Tuple =>
        tupleToData(_values)
 
       case caseclass: Product =>
         val tuple = Tuple.fromProduct(caseclass)
         tupleToData(tuple)
-
-      case _ => Map.empty
     }
-    copy(data= data)
+    copy(mappings = filledFields)
 
   def bindFromRequest()(using request: com.greenfossil.webserver.Request): Form[A] =
     val querydata: Map[String, Seq[String]] =
@@ -96,28 +99,39 @@ case class Form[A](mappings: Tuple, data: Map[String, Any] = Map.empty , errors:
       }
     request match {
       case req if req.asFormUrlEncoded.nonEmpty =>
-        bind(req.asFormUrlEncoded ++querydata)
+        bind(req.asFormUrlEncoded ++ querydata)
+
       case req if req.asMultipartFormData.bodyPart.nonEmpty =>
         bind(req.asMultipartFormData.asFormUrlEncoded ++ querydata)
+
       case req if req.asJson.asOpt.isDefined =>
         bind(req.asJson, querydata)
     }
 
   def bind(data: Map[String, Seq[String]]): Form[A] = {
-      ???
+    val newMappings = mappings.map[[A] =>> Field[_]] {
+      [X] => (x: X) => x match
+        case f: Field[t] => f.copy(value = Field.toValueOf(f.tpe, data.get(f.name).orNull))
+    }
+    this.copy(mappings = newMappings, data = data)
   }
 
   def bind(js: JsValue, query: Map[String, Seq[String]]): Form[A] = {
-    ???
+    //WIP
+    val newMappings = mappings.map[[A] =>> Field[_]] {
+      [X] => (x: X) => x match
+        case f: Field[t] => f.copy(value = Field.toValueOf(f.tpe, (js \ f.name).asOpt[String]))
+    }
+    this.copy(mappings = newMappings, data = null)
   }
 
-  private def tupleToData(values: Tuple): Map[String, Any] = {
-    val xs = (0 until mappings.productArity) .map { i =>
-      val (name: String, f) = mappings.productElement(i)
-      val value = values.productElement(i)
-      (name,value)
-    }
-    xs.toMap
+  private def tupleToData(values: Product): Field[_] *: Tuple = {
+    val valuesIter = values.productIterator
+    val filledFields = mappings.map[[F] =>> Field[_]](
+      [F] => (f: F) => f match {
+        case f: Field[_] => f.copy(value = valuesIter.nextOption())
+      })
+    filledFields
   }
 
   def fold[R](hasErrors: Form[A] => R, success: A => R): R = value match {
@@ -125,25 +139,83 @@ case class Form[A](mappings: Tuple, data: Map[String, Any] = Map.empty , errors:
     case _ => hasErrors(this)
   }
 
-  def apply(key: String): Field[_] =
-    Field(this, key, Nil, data.get(key))
+  inline def apply[T](key: String): Field[T] =
+    mappings
+      .productIterator
+      .find(_.asInstanceOf[Field[T]].name == key)
+      .map(_.asInstanceOf[Field[T]])
+      .orNull
 
-}
-
-trait FieldMapper {
-  def of[A](name:String): Field[A]
 }
 
 object Field {
-  def apply[A](name: String): Field[A] =
-    Field(null, name, Nil, None)
+  import scala.compiletime.*
+
+  inline def of[A]: Field[A] = Field(fieldType[A])
+
+  inline def of[A](name: String): Field[A] = Field(fieldType[A], name = name)
+
+  inline def fieldType[A]: String =
+    inline erasedValue[A] match {
+      case _: Int       => "Int"
+      case _: String    => "String"
+      case _: Long      => "Long"
+      case _: Seq[a]    => "[" + fieldType[a]
+      case _: Option[a] => "?" + fieldType[a]
+    }
+
+  def toValueOf[A](tpe: String, value: Any): Option[A] = {
+    if value == null then None
+    else {
+      val optValue = tpe match {
+        case "Int" =>
+          value match {
+            case x: Int => Option(x)
+            case s: String => s.toIntOption
+            case xs: Seq[_] => xs.headOption.flatMap(_.toString.toIntOption)
+            case _ => None
+          }
+
+        case "Long" =>
+          value match {
+            case x: Long => Option(x)
+            case s: String => s.toLongOption
+            case xs: Seq[_] => xs.headOption.flatMap(_.toString.toLongOption)
+            case _ => None
+          }
+
+        case "String" =>
+          value match {
+            case s: String => Option(s)
+            case xs: Seq[_] => xs.headOption.map(_.toString)
+            case _ => None
+          }
+        case seq if seq.startsWith("[") =>
+          value match {
+            case xs: Seq[_] =>
+              Option(xs.flatMap(x => toValueOf(seq.tail,x)))
+            case s: String =>
+              Option(Seq(toValueOf(seq.tail, s)))
+          }
+        case opt if opt.startsWith("?") =>
+          value match {
+            case xs: Seq[_] => Option(xs.flatMap(x => toValueOf(opt.tail,x)))
+            case s: String => Option(Seq(toValueOf(opt.tail, s)))
+          }
+      }
+      optValue.asInstanceOf[Option[A]]
+    }
+  }
+
 }
 
-case class Field[A](form: Form[_], name: String, errors: Seq[FormError], value: Option[A])
+case class Field[A](tpe: String, form: Form[_] = null , name: String = null, errors: Seq[FormError] = Nil, value: Option[A] = None) {
+  def setValue(a: A): Field[A] = copy(value = Option(a))
+}
 
-def longNumber: Field[Long] = Field("Long")
-def text: Field[String] = Field("String")
-def seq[A] : Field[Seq[A]] = Field("Seq")
-def optional[A] : Field[Option[A]] = Field("Option")
+inline def longNumber = Field.of[Long]
+inline def text = Field.of[String]
+inline def seq[A] = Field.of[Seq[A]]
+inline def optional[A] = Field.of[Option[A]]
 
 case class FormError(key: String, messages: Seq[String], args: Seq[Any] = Nil)
--- src/test/scala/com/greenfossil/webserver/FormSuite.scala
+++ src/test/scala/com/greenfossil/webserver/FormSuite.scala
@@ -1,6 +1,6 @@
 package com.greenfossil.webserver
 
-import com.greenfossil.webserver.data.*
+import com.greenfossil.webserver.data.{*, given}
 
 class FormSuite extends munit.FunSuite {
 
@@ -10,37 +10,52 @@ class FormSuite extends munit.FunSuite {
       "text" -> text
     )
     val filledForm = form.fill(1, "hello")
-    assertEquals(filledForm.data.size, 2)
-    assertEquals(filledForm.data("long"), 1)
-    assertEquals(filledForm.data("text"), "hello")
+//    assertEquals(filledForm.data.size, 2)
+//    assertEquals(filledForm.data("long"), 1)
+//    assertEquals(filledForm.data("text"), "hello")
     assertEquals[Any, Any](filledForm("long").value, Option(1))
     assertEquals[Any, Any](filledForm("text").value, Option("hello"))
   }
 
-  test("tuple") {
+  test("fill tuple") {
     val form: Form[(Long, String, Seq[Long])] = Form.asTuple(
       "long" -> longNumber,
       "text" -> text,
       "seq" -> seq[Long]
     )
     val filledForm = form.fill(1, "hello", Seq(1,2))
-    assertEquals(filledForm.data.size, 3)
-    assertEquals(filledForm.data("long"), 1)
-    assertEquals(filledForm.data("text"), "hello")
-    assertEquals(filledForm.data("seq"), Seq(1,2))
+//    assertEquals(filledForm.data.size, 3)
+    assertEquals[Any, Any](filledForm("long").value, Option(1))
+    assertEquals[Any, Any](filledForm("text").value, Option("hello"))
+    assertEquals[Any, Any](filledForm("seq").value, Option(Seq(1,2)))
   }
 
-  test("case class 2") {
-    case class Foo(l: Long, s: String)
-    val form: Form[Foo] = Form.asClass[Foo](
+  test("bind tuple 2".only) {
+    val form: Form[(Long, String, Seq[Int])] = Form.asTuple(
       "long" -> longNumber,
-      "text" -> text
+      "text" -> text,
+      "seq" -> seq[Int]
     )
-    val filledForm = form.fill(Foo(1, "hello"))
-    assertEquals(filledForm.data.size, 2)
-    assertEquals(filledForm.data("long"), 1)
-    assertEquals(filledForm.data("text"), "hello")
+    val bindedForm = form.bind(Map("long" -> Seq("1"), "text" -> Seq("hello"), "seq" -> Seq("1", "2")))
+    assertEquals(bindedForm.data.size, 3)
+    val a = bindedForm("long")
+    val x = bindedForm("long").value
+    assertEquals[Any, Any](bindedForm("long").value, Option(1))
+    assertEquals[Any, Any](bindedForm("text").value, Option("hello"))
+    assertEquals[Any, Any](bindedForm("seq").value, Option(Seq(1,2)))
   }
+
+//  test("case class 2") {
+//    case class Foo(l: Long, s: String)
+//    val form: Form[Foo] = Form.asClass[Foo](
+//      "long" -> longNumber,
+//      "text" -> text
+//    )
+//    val filledForm = form.fill(Foo(1, "hello"))
+//    assertEquals(filledForm.data.size, 2)
+//    assertEquals(filledForm.data("long"), 1)
+//    assertEquals(filledForm.data("text"), "hello")
+//  }
 //
 //  test("case class 3") {
 //    case class Foo(l: Long, s: String, xs: Seq[Long])
