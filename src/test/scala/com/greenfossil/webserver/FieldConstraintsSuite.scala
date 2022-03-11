86ed4f40e4e064f68415f68e01111a090386fae0
Phuong Vu <phuong@greenfossil.com>
2022-03-11 14:37:05 +0800

    FieldConstraintsSuite - WIP

--- src/test/scala/com/greenfossil/webserver/FieldConstraintsSuite.scala
+++ src/test/scala/com/greenfossil/webserver/FieldConstraintsSuite.scala
@@ -1,9 +1,49 @@
 package com.greenfossil.webserver
 
+import com.greenfossil.webserver.data.*
+
 class FieldConstraintsSuite extends munit.FunSuite {
   
   test("nonEmptyText") {
-    
+    val f = nonEmptyText
+    val errorField = f.bind("")
+    assertEquals(errorField.errors.size, 1)
+    assertEquals(errorField.errors.head.message, "error.required")
+    errorField.errors foreach println
+
+    val validField = f.bind("abc")
+    assert(validField.errors.isEmpty)
   }
 
+  test("email"){
+
+  }
+
+  test("number"){
+
+  }
+
+  test("text"){
+
+  }
+
+  test("byte number"){
+
+  }
+
+  test("short number"){
+
+  }
+
+  test("long number"){
+
+  }
+
+  test("big decimal"){
+
+  }
+
+  test("custom constraint"){
+
+  }
 }
