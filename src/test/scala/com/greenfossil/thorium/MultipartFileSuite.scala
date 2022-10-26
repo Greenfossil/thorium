ea05b442054aabaa05ed98d4a6113c788091ed6f
Phuong Vu <phuong@greenfossil.com>
2022-10-26 10:50:07 +0800

    add copyright

--- src/test/scala/com/greenfossil/thorium/MultipartFileSuite.scala
+++ src/test/scala/com/greenfossil/thorium/MultipartFileSuite.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2022 Greenfossil Pte Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.greenfossil.thorium
 
 import com.linecorp.armeria.common.MediaType
@@ -33,7 +49,7 @@ class MultipartFileSuite extends munit.FunSuite {
     assert(r != null, "Resource not found")
     val mpFile = MultipartFile.of("file", "file", new File(r.getFile))
     assert(mpFile.contentType.is(MediaType.parse("application/xml")))
-    assertEquals(mpFile.sizeInBytes, 473L)
+    assertEquals(mpFile.sizeInBytes, 1094L)
   }
 
   test("MultipartFile extensions for conf") {
@@ -42,7 +58,7 @@ class MultipartFileSuite extends munit.FunSuite {
     val mpFile = MultipartFile.of("file", "file", new File(r.getFile))
     println(s"mpFile.contentType = ${mpFile.contentType}")
     assert(mpFile.contentType.is(MediaType.parse("application/xml")))
-    assertEquals(mpFile.sizeInBytes, 473L)
+    assertEquals(mpFile.sizeInBytes, 1094L)
   }
 
   test("Multipart inputStream") {
