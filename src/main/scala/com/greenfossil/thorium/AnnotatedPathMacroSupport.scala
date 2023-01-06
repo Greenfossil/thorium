6f8830962ef90c137349ec76030b39d9afa5e15b
Cheong Chung Onn <chungonn@greenfossil.com>
2023-01-06 09:57:44 +0800

    Bump version 0.6.4

--- src/main/scala/com/greenfossil/thorium/AnnotatedPathMacroSupport.scala
+++ src/main/scala/com/greenfossil/thorium/AnnotatedPathMacroSupport.scala
@@ -76,10 +76,10 @@ object AnnotatedPathMacroSupport extends MacroSupport(globalDebug = false):
         onSuccessCallback(Expr("Get"), Expr.ofList(List(idTerm.asExpr)), Expr[List[String]](Nil), Expr.ofList(List.empty[Expr[Any]]), Expr(""))
 
       case otherTerm =>
-        show("otherTerm", otherTerm, true)
+        show("otherTerm", otherTerm)
         val ref = Ref(otherTerm.symbol)
         val term = searchForAnnotations(ref, 1)
-        show("search term", term, true)
+        show("search term", term)
         report.errorAndAbort("Unable to find any annotated path")
 
   /**
